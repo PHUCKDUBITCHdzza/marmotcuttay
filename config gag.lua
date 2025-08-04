cc4b32630c13989adfdaa0e14ea06bc528425e53
DuongLucPhi <cholacoho123456789@gmail.com>
2025-08-04 23:21:06 +0700

    Update config gag.lua

--- config gag.lua
+++ config gag.lua
@@ -1,5 +1,5 @@
 wait(15)
-script_key="GMyqZfwglsTgGOMQwEcdfTFNjOvRZhQc";
+script_key="lJBojjkFThlLaVvGlDhQeHueHNWeUTHa";
 setfpscap(3)
 
 getgenv().gagConfig = {
@@ -55,5 +55,6 @@ getgenv().gagConfig = {
 }
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c916e5b90dc37c71ecf1ec00dfce3d5d.lua"))()
+
 
 
