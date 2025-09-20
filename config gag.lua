ca605f3dcf68f3c4dc5949b269eb88cb3d88724c
PhockYouBitch <cholacoho123456789@gmail.com>
2025-09-21 00:28:53 +0700

    Update config gag.lua

--- config gag.lua
+++ config gag.lua
@@ -19,7 +19,7 @@ getgenv().gagConfig = {
     -- Event:
     CRAFT_EVENT = { "Anti Bee Egg", "Skyroot Chest" , "Sprout Egg" },
     BUY_TRAVELING_MERCHANT = { "Bee Egg", "Paradise Egg", "Rare Summer Egg", "Common Summer Egg", "Pitcher Plant", "Feijoa", "Loquat" },
-    BUY_EVENT_SHOP = { "Fall Egg" },
+    BUY_EVENT_SHOP = { "Fall Egg", "Red Panda" },
     
     PLANT_EVENT_TREES = true,  -- This config will replace most plant related config
     PLACE_COSMETIC = { "Cooking Kit" },
@@ -184,3 +184,4 @@ task.spawn(function()
         harvestAndSubmit2()
     end
 end)
+
